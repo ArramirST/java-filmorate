32e36e954fc3f6d118e486113d5514c4946a73dc
Ruslan <ruslanmarkov96@yandex.ru>
2023-12-11 00:41:25 +0300

    ver.1.4.2

--- src/test/java/ru/yandex/practicum/controller/UserControllerTests.java
+++ src/test/java/ru/yandex/practicum/controller/UserControllerTests.java
@@ -2,13 +2,10 @@ package ru.yandex.practicum.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.exeptions.ValidationException;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.time.Duration;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
